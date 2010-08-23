8a76cf5cdc1ea40a9b2200865846ba58c82203cd
Brian Cowdery <bcowdery@pointyspoon.com>
2010-08-23 17:24:04 +0000

    Added JAXB XML marshal/unmarshal for base entities

--- billing-ng-core/src/main/java/com/billing/ng/entities/BaseEntity.java
+++ billing-ng-core/src/main/java/com/billing/ng/entities/BaseEntity.java
@@ -17,12 +17,25 @@
 
 package com.billing.ng.entities;
 
+import com.billing.ng.xml.XmlNamespacePrefixMapper;
+import com.sun.xml.bind.marshaller.NamespacePrefixMapper;
 import org.hibernate.validator.ClassValidator;
 import org.hibernate.validator.InvalidValue;
 
 import javax.persistence.Transient;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 import javax.xml.bind.annotation.XmlTransient;
+import javax.xml.transform.stream.StreamSource;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * BaseEntity
@@ -32,6 +45,24 @@ import java.io.Serializable;
  */
 @XmlTransient
 public abstract class BaseEntity implements Serializable {
+
+    /**
+     * colon-separates string of packages that contain JAXB bound objects.
+     */
+    private static transient String PACKAGES;
+    static {
+        List<String> packages = Arrays.asList(
+                "com.billing.ng.entities"
+        );
+
+        StringBuilder builder = new StringBuilder();
+        for (Iterator<String> it = packages.iterator(); it.hasNext();) {
+            String pkg = it.next();
+            builder.append(pkg);
+            if (it.hasNext()) builder.append(":");
+        }
+        PACKAGES = builder.toString();
+    }
 
     /**
      * Executes the Hibernate ClassValidator on this class, validating
@@ -44,5 +75,48 @@ public abstract class BaseEntity implements Serializable {
     public InvalidValue[] validate() {
         ClassValidator validator = new ClassValidator(this.getClass());
         return validator.getInvalidValues(this);
+    }
+
+    /**
+     * Marshals this entity to an XML string.
+     *
+     * @return this entity in a serialized XML form.
+     * @throws JAXBException thrown if JAXB context cannot be initialized
+     * @throws IOException thrown if serialization fails
+     */
+    @Transient
+    public String toXml() throws JAXBException, IOException {
+        JAXBContext context = JAXBContext.newInstance(PACKAGES);
+        Marshaller m = context.createMarshaller();
+        m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        m.setProperty("com.sun.xml.bind.namespacePrefixMapper", new XmlNamespacePrefixMapper());
+
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+        m.marshal(this, bos);
+
+        bos.flush();
+        bos.close();
+
+        return bos.toString();
+    }
+
+    /**
+     * Un-marshals an entity from a given XML string. 
+     *
+     * @param destType destination entity type
+     * @param xml serialized XML form to read
+     * @param <T> Entity type of un-marshaled entity
+     * @return un-marshaled entity
+     * @throws JAXBException thrown if JAXB context cannot be initialized
+     * @throws IOException thrown if serialized XML cannot be read
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> T fromXml(Class<T> destType, String xml) throws JAXBException, IOException {
+        JAXBContext context = JAXBContext.newInstance(PACKAGES);
+        Unmarshaller u = context.createUnmarshaller();
+
+        ByteArrayInputStream bis = new ByteArrayInputStream(xml.getBytes());
+        return (T) u.unmarshal(new StreamSource(bis));
     }
 }
