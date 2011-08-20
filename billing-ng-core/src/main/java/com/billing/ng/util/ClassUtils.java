43ad38b538e421b132fd75a8f29a2652280f93be
Brian Cowdery <brianc@jbilling.com>
2011-08-19 23:06:56 -0600

    Class utils for classloading in a webapp

---
+++ billing-ng-core/src/main/java/com/billing/ng/util/ClassUtils.java
@@ -0,0 +1,56 @@
+/*
+ BillingNG, a next-generation billing solution
+ Copyright (C) 2011 Brian Cowdery
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as
+ published by the Free Software Foundation, either version 3 of the
+ License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see http://www.gnu.org/licenses/agpl-3.0.html
+ */
+
+package com.billing.ng.util;
+
+/**
+ * ClassUtils
+ *
+ * @author Brian Cowdery
+ * @since 19-08-2011
+ */
+public class ClassUtils {
+
+    /**
+     * Returns the <code>Class</code> object associated with the class or
+     * interface with the given string name.
+     *
+     * This method attempts to load the class using the current thread context
+     * class loader before the local caller class loader in case the target class
+     * has been loaded by a different context (e.g, a web-application).
+     *
+     * @param className class name to load
+     * @return class
+     * @throws ClassNotFoundException thrown if class is not found
+     */
+    public static Class forName(String className) throws ClassNotFoundException {
+        try {
+            ClassLoader loader = Thread.currentThread().getContextClassLoader();
+            return loader.loadClass(className);
+
+        } catch (ClassNotFoundException e) {
+            try {
+                return Class.forName(className);
+            } catch (ClassNotFoundException ignore) {
+                /* ignore and rethrow original */
+            }
+
+            throw e;
+        }
+    }
+
+}
